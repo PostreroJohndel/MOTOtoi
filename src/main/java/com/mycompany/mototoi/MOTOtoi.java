37bf6eeb27de0ad0bc6d51fbff20b10c4e5ff309
GelaPostrero <angelapostrero@gmail.com>
2024-11-29 13:41:28 +0800

    first commit

---
+++ src/main/java/com/mycompany/mototoi/MOTOtoi.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mototoi;
+
+/**
+ *
+ * @author Angela
+ */
+public class MOTOtoi {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
